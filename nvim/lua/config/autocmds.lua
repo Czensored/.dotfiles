15d39ee8556a9ccad4680e67197326f03d6dfeb1
Isaac-Salzman <37640727+Czensored@users.noreply.github.com>
2025-06-18 09:26:24 +0900

    Fix: remove nvim as submodule and add as normal directory

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,21 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Auto-save current buffer on various "focus lost" events
+-- Auto-save on FocusLost, BufLeave, or TermOpen
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave", "TermOpen" }, {
+  pattern = "*",
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    if vim.bo[buf].modifiable and vim.bo[buf].modified and vim.bo[buf].buftype == "" then
+      vim.cmd("silent! write")
+    end
+  end,
+  desc = "Auto-save current buffer on focus loss, buffer switch, or terminal open",
+})
+
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,5 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- vim.keymap.set("i", "jj", "<Esc>", { desc = "Escape insert mode", noremap = false, silent = true })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
